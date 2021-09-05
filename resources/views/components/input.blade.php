de8cbeeefa79627370395e9a792edfe7c06b22a6
phu1237 <phu1237@gmail.com>
2021-09-05 23:18:58 +0700

    fix component

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,10 +1,5 @@
-<input name="{{ $key }}"
-    id="{{ $key }}"
-    type="{{ $meta->type ?? 'text' }}"
-    class="{{ $class }}"
-    {{ $this->style != null ? 'style="'.$style.'"' : ''}
-    {!! !empty($meta->placeholder) ? 'placeholder="'.$meta->placeholder.'"' : '' !!}
-    value="{{ $value ?? '' }}"
-    {{ isset($meta->required) && $meta->required === true ? 'required' : '' }}
-    {{ $locked === true ? 'readonly' : '' }}
+<input name="{{ $key }}" id="{{ $key }}" type="{{ $meta->type ?? 'text' }}" class="{{ $class }}" {{ $this->style != null ? 'style="'.$style.'"' : ''}} {!! !empty($meta->placeholder) ? 'placeholder="'.$meta->placeholder.'"' : '' !!}
+value="{{ $value ?? '' }}"
+{{ isset($meta->required) && $meta->required === true ? 'required' : '' }}
+{{ $locked === true ? 'readonly' : '' }}
 >
